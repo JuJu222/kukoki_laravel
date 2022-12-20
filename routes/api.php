c6ab004f50a537cb24901493facbcddfb90a3522
jxnhiro <kenny.jinhiro@gmail.com>
2022-12-20 08:16:03 +0700

    Added basic User API :+1:

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\CheckoutController;
+use App\Http\Controllers\API\UserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/checkout', [CheckoutController::class, 'getSnapToken']);
+
+Route::get('/user', [UserController::class, 'index']);
